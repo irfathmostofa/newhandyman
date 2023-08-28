56a87e378aa70dc26f8dfd340cda19198a9f96fe
FK-R <fakwer.uddin@gmail.com>
2023-08-29 03:13:55 +0600

    icon boshailam nav banailam responsive baki

--- pages/components/Layout/Footer.jsx
+++ pages/components/Layout/Footer.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 export const metadata = {
   url: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css",
 };
@@ -7,98 +8,112 @@ const Footer = () => {
   return (
     <>
       {" "}
-      <footer class="site-footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
+      <footer className="site-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
               <h5>
                 {" "}
-                <i class="fas fa-tools"></i> Handyman Service
+                <span className="fs-4"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-wrench-adjustable-circle" viewBox="0 0 16 16">
+  <path d="M12.496 8a4.491 4.491 0 0 1-1.703 3.526L9.497 8.5l2.959-1.11c.027.2.04.403.04.61Z"/>
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1 0a7 7 0 1 0-13.202 3.249l1.988-1.657a4.5 4.5 0 0 1 7.537-4.623L7.497 6.5l1 2.5 1.333 3.11c-.56.251-1.18.39-1.833.39a4.49 4.49 0 0 1-1.592-.29L4.747 14.2A7 7 0 0 0 15 8Zm-8.295.139a.25.25 0 0 0-.288-.376l-1.5.5.159.474.808-.27-.595.894a.25.25 0 0 0 .287.376l.808-.27-.595.894a.25.25 0 0 0 .287.376l1.5-.5-.159-.474-.808.27.596-.894a.25.25 0 0 0-.288-.376l-.808.27.596-.894Z"/>
+</svg></span> Handyman Service 
               </h5>
-              <p class="text-justify">
-                {/* "Reliable Handyman Services in Singapore - Your Trusted Partner */} {/* FIX */}
+              <p className="text-justify">
+                Reliable Handyman Services in Singapore - Your Trusted Partner {/* FIX */}
                 for Quality Repairs and Maintenance.{" "}
               </p>{" "}
             </div>
 
-            <div class="col-xs-6 col-md-3">
+            <div className="col-xs-6 col-md-3">
               <h6>Contacts</h6>
-              <ul class="footer-links">
+              <ul className="footer-links">
                 <li>
                   {" "}
-                  <i className="fas fa-home mb-2"></i>{" "}
-                  <a href="#">Singapore 209560</a>
+                  <i className="fs-4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house-door" viewBox="0 0 16 16">
+  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
+</svg></i>{" "}
+                  <Link href="#">Singapore 209560</Link> 
                 </li>
                 <li>
                   {" "}
-                  <i className="fas fa-envelope mb-2"></i>{" "}
-                  <a href="mailto:kazi2630mt@gmail.com">
+                  <i className="fs-4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-at" viewBox="0 0 16 16">
+  <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+  <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+</svg></i>{" "}
+                  <Link href="mailto:kazi2630mt@gmail.com">
                     {" "}
                     kazi2630mt@gmail.com
-                  </a>
+                  </Link> 
                 </li>
                 <li>
                   {" "}
-                  <i className="fas fa-phone mb-2"></i>{" "}
-                  <a href="tel:+65 8596 6244">+65 8596 6244</a>
+                  <i className="fs-4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-telephone-forward" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zm10.762.135a.5.5 0 0 1 .708 0l2.5 2.5a.5.5 0 0 1 0 .708l-2.5 2.5a.5.5 0 0 1-.708-.708L14.293 4H9.5a.5.5 0 0 1 0-1h4.793l-1.647-1.646a.5.5 0 0 1 0-.708z"/>
+</svg></i>{" "}
+                  <Link href="tel:+65 8596 6244">+65 8596 6244</Link> 
                 </li>
-                <li>
-                  {" "}
-                  <i className="fas fa-phone mb-2"></i>{" "}
-                  <Link
-                    class="facebook"
-                    target="_blank"
-                    href="https://www.facebook.com/profile.php?id=100072178856281"
-                  >
-                    facebook
-                  </Link>
-                </li>
-                <li>
-                  {" "}
-                  <i className="fas fa-phone mb-2"></i>{" "}
-                  <Link
-                    class="dribbble"
-                    target="_blank"
-                    href="https://wa.link/xqqjco"
-                  >
-                    whatsapp
-                  </Link>
-                </li>
+          
               </ul>
             </div>
 
-            <div class="col-xs-6 col-md-3">
+            <div className="col-xs-6 col-md-3">
               <h6>Quick Links</h6>
-              <ul class="footer-links">
+              <ul className="footer-links">
                 <li>
-                  {/* <a href="/about">About Us</a> */} {/* FIX */}
+                  <Link href="/about">About Us</Link>
                 </li>
                 <li>
-                  {/* <a href="/contact">Contact Us</a> */} {/* FIX */}
+                 <Link href="/contact">Contact Us</Link>
                 </li>
                 <li>
-                  <a href="/services">Services</a>
+                  <Link href="/service">Services</Link>
                 </li>
+         
               </ul>
             </div>
           </div>
           <hr />
         </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8 col-sm-6 col-xs-12">
-              <small class="copyright-text">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-sm-6 col-xs-12">
+              <small className="copyright-text">
                 Copyright &copy; 20123 All Rights Reserved by
                 <Link href="/"> HandymanAHM</Link>.
               </small>{" "}
               <br />
-              <small class="copyright-text">
+              <small className="copyright-text">
                 Developed By
                 <Link href="https://www.linkedin.com/in/fkr-uddin/"> FKR</Link>.
               </small>
             </div>
 
-            <div class="col-md-4 col-sm-6 col-xs-12"></div>
+            <div className="col-md-4 col-sm-6 col-xs-12">
+
+           <div className="social-icons">
+           <Link
+                    className="facebook"
+                    target="_blank"
+                    href="https://www.facebook.com/profile.php?id=100072178856281"
+                  >
+                     <i className=""><svg className="pb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+</svg> </i>
+                  </Link>
+            
+               
+                  <Link
+                    className="dribbble"
+                    target="_blank"
+                    href="https://wa.link/xqqjco"
+                  >
+                      <i className=""><svg className="pb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-whatsapp" viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+</svg></i>{" "}
+                  </Link>
+           </div>
+            </div>
           </div>
         </div>
       </footer>
--- pages/index.jsx
+++ pages/index.jsx
@@ -34,7 +34,7 @@ export default function Home() {
         />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/ahm.ico" />
       </Head>
       <main>
         <Navber />
